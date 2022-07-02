49184924c53f048ecfd14db1aa6da4093e86612b
王心凌 <maple@lihuia.com>
2022-07-02 14:07:00 +0800

    添加测试页，删除冗余页面

--- config/routes.ts
+++ config/routes.ts
@@ -14,37 +14,16 @@
     ],
   },
   {
-    path: '/welcome',
-    name: 'welcome',
-    icon: 'smile',
-    component: './Welcome',
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    icon: 'crown',
-    access: 'canAdmin',
-    routes: [
-      {
-        path: '/admin/sub-page',
-        name: 'sub-page',
-        icon: 'smile',
-        component: './Welcome',
-      },
-      {
-        component: './404',
-      },
-    ],
-  },
-  {
     name: 'list.table-list',
     icon: 'table',
     path: '/list',
     component: './TableList',
   },
   {
-    path: '/',
-    redirect: '/welcome',
+    name: 'test',
+    path: '/test',
+    icon: 'table',
+    component: './Test',
   },
   {
     component: './404',
