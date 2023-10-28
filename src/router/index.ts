dd5f0ebf906a23aba12fc1d43ce7c44f08aa2935
王心凌 <maple@lihuia.com>
2023-10-28 20:22:21 +0800

    初始化项目

---
+++ src/router/index.ts
@@ -0,0 +1,63 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import { usePermissStore } from '../store/permiss';
+import Home from '../views/home.vue';
+
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        redirect: '/user',
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+        children: [
+            {
+                path: '/user',
+                name: 'User',
+                meta: {
+                    title: '用户管理',
+                    permiss: '1',
+                },
+                component: () => import('../views/user.vue'),
+            },
+        ],
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        meta: {
+            title: '登录',
+        },
+        component: () => import(/* webpackChunkName: "login" */ '../views/login.vue'),
+    },
+    {
+        path: '/403',
+        name: '403',
+        meta: {
+            title: '没有权限',
+        },
+        component: () => import(/* webpackChunkName: "403" */ '../views/403.vue'),
+    },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+router.beforeEach((to, from, next) => {
+    document.title = `${to.meta.title} | mysterious`;
+    const role = localStorage.getItem('ms_username');
+    const permiss = usePermissStore();
+    if (!role && to.path !== '/login') {
+        next('/login');
+    } else if (to.meta.permiss && !permiss.key.includes(to.meta.permiss)) {
+        // 如果没有权限，则进入403
+        next('/403');
+    } else {
+        next();
+    }
+});
+
+export default router;
---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+export default defineConfig({
+	base: './',
+	plugins: [
+		vue(),
+		VueSetupExtend(),
+		AutoImport({
+			resolvers: [ElementPlusResolver()]
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()]
+		})
+	],
+	optimizeDeps: {
+		include: ['schart.js']
+	},
+	server: {
+		host: 'locahost',
+		port: 8888,
+		open: true,
+		// 设置代理
+		proxy: {
+		'/mysterious': {
+			target: 'http://127.0.0.1:9999',
+			changeOrigin: true,
+			rewrite: path => path.replace(/^\/mysterious/, '')
+			},
+		}
+	}
+});
