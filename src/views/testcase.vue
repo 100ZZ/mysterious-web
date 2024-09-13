9ab8acfc09eba82f4850d6696ce2423d60ac11cc
lihui <maple@lihuia.com>
2024-09-13 18:12:25 +0800

    优化param和body互斥

--- src/views/testcase.vue
+++ src/views/testcase.vue
@@ -1543,10 +1543,26 @@ const updateOnlineJmxData = async (formattedItem: OnlineJmxItemForApi) => {
 
 // 处理保存操作的函数
 const handleSave = () => {
-  console.log("before format: ", onlineJmxItem.value);
+  // 强制计算 isParamDisabled 和 isBodyDisabled 确保它们的值是最新的
+  const isParamDisabledValue = isParamDisabled.value;
+  const isBodyDisabledValue = isBodyDisabled.value;
+
+  // console.log("before format: ", onlineJmxItem.value);
   const formattedItem = convertBooleanToNumber(onlineJmxItem.value);
-  console.log("after format: ", formattedItem);
-  let res;
+  // console.log("after format: ", formattedItem);
+
+  // console.log("isParamDisabled:", isParamDisabled.value);
+  // console.log("isBodyDisabled:", isBodyDisabled.value);
+  // 如果 Param 被禁用，则将 Param 传 null
+  if (isParamDisabledValue) {
+    formattedItem.httpVO.httpParamVOList = [];
+  }
+
+  // 如果 Body 被禁用，则将 Body 传 null
+  if (isBodyDisabledValue) {
+    formattedItem.httpVO.body = null;
+  }
+
   // 保存编辑后的 JMX 脚本数据
   if (onlineJmxItem.value.id) {
     // 如果 id 不为空，则调用更新操作
