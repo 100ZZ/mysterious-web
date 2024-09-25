f9d68d6b5f67cc2a40c47c53f1334f2cc73fb3e2
lihui <maple@lihuia.com>
2024-09-25 09:53:31 +0800

    update style

--- src/views/jmx.vue
+++ src/views/jmx.vue
@@ -19,7 +19,7 @@
         <el-table-column prop="modifier" label="修改人" align="center"></el-table-column>
         <el-table-column prop="modifyTime" label="修改时间" align="center"></el-table-column>
 
-        <el-table-column label="操作" width="120" align="center">
+        <el-table-column label="操作" width="220" align="center">
           <template #default="scope">
             <el-button text :icon="Search" class="bg-blue" @click="drawer = true,handleJmxView(scope.row.id)" v-permiss="1">
               预览
