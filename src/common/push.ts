c98cb278d640d30971c73ffd86264b3aaa384ff4
lihui <maple@lihuia.com>
2024-10-13 16:11:18 +0800

    Fix: 如果用户被删除，该用户登录默认跳转到登录页面

--- src/common/push.ts
+++ src/common/push.ts
@@ -7,6 +7,13 @@ export const checkToLogin = (message: String) => {
     }
 }
 
+export const checkUserExist = (message: String) => {
+    if (message == '用户不存在') {
+        router.push({path: '/login'});
+        return false;
+    }
+}
+
 export const handleTestCaseClick = (testCaseId: number) => {
     // 使用路由跳转到用例页面，并传递用例ID作为查询参数
     router.push({
